0176db9da379e9a1f04e39c38009fed37caa863f
Mark Morgan <Mark.Morgan@perfect-image.co.uk>
2022-02-09 12:09:28 +0000

    Create Item Model With EntityFramework Support 2

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -10,5 +10,10 @@ namespace WishList.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Item> Items { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+
+        }
     }
 }
--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -12,6 +12,6 @@ namespace WishList.Models
 
 
         [Required, MaxLength(50)]
-        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
